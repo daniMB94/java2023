9b2d0d5cdcda1ef123dc34d4d04a53f63118fe87
usuario1DAW <dani_aliast@hotmail.com>
2022-11-10 21:51:02 +0100

    primer ejercicio

--- PruebaGithub/src/Prueba/Prueba.java
+++ PruebaGithub/src/Prueba/Prueba.java
@@ -3,7 +3,27 @@ package Prueba;
 public class Prueba {
 
 	public static void main(String[] args) {
-		System.out.println("Este fichero va a aparecer en Github 232");
+
+
+		//Declaración de variables
+		//TIPO -> NOMBRE = VALOR POR DEFECTO O INICIAL
+		
+		short num1 = 8;
+		int mes = 12;
+		long distancia = 3000000;
+		char letra = 'a';
+		float decimal = 1e10f;
+		double precio = 35000.95;
+		
+		boolean ganasDeIrme = true;
+		
+		System.out.println(num1);
+		System.out.println(mes);
+		System.out.println(distancia);
+		System.out.println(letra);
+		System.out.println(decimal);
+		System.out.println(precio);
+		System.out.println(ganasDeIrme);
 		
 	}
 
