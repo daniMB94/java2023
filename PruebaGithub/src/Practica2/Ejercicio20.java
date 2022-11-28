98c79e601b281cc1e9dbb9f87bb70551f18eb109
dani_ <dani_aliast@hotmail.com>
2022-11-28 10:40:58 +0100

    20221128 1

--- PruebaGithub/src/Practica2/Ejercicio20.java
+++ PruebaGithub/src/Practica2/Ejercicio20.java
@@ -11,7 +11,10 @@ public class Ejercicio20 {
 		x = 5 * Math.pow(a, 2) * Math.pow(b, 3) + (Math.pow((Math.pow(a, 2)+ Math.pow(b, 2)), 1/2));
 		
 		//2)
-		x = 
+		x = Math.pow((4 * a * Math.pow(b,2)), 1/2);
+		
+		//3)
+		x = (((Math.pow(a, 3) * b) / (2 * a * Math.pow(b, 2))) - Math.pow((12 * Math.pow(d, 4)), 1/2));
 		
 	}
 
