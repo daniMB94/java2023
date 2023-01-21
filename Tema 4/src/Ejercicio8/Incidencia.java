b2e60834eaa2e5d41edd50eaa31f99cb2686b0c1
dani_ <dani_aliast@hotmail.com>
2023-01-21 18:55:10 +0100

    20230121 otro más

--- Tema 4/src/Ejercicio8/Incidencia.java
+++ Tema 4/src/Ejercicio8/Incidencia.java
@@ -6,41 +6,42 @@ public class Incidencia {
 	private String estado;
 	private String problema;
 	private String solucion;
-	
+
 	public static int pendientes = 0;
 
-	//CONSTRUCTOR CON PARAMETROS
-	public Incidencia(String problema, String solucion) {
+	// CONSTRUCTOR CON PARAMETROS
+	public Incidencia(String problema) {
 		super();
 		this.codigo = generarCodigo();
 		this.estado = "Abierta";
 		this.problema = problema;
-		this.solucion = solucion;
+		this.solucion = "Por determinar";
 		Incidencia.pendientes++;
 	}
-	//METODOS
+
+	// METODOS
 	private String generarCodigo() {
-		
+
 		StringBuilder sb = new StringBuilder();
 		sb.append("inc");
-		for(int i = 0; i < 5; i++) {
-			sb.append( (int) Math.random()*10);
+		for (int i = 0; i < 5; i++) {
+			sb.append((int) Math.random() * 10);
+		}
+
+		return sb.toString();
 	}
-	
-	return sb.toString();
-	}
-	
-	public void resuelve() {
-		
-		if (this.estado != "Resuleta") {
+
+	public void resuelve(String solucion) {
+
+		if (this.estado != "Resuelta") {
 			this.estado = "Resuelta";
+			this.solucion = solucion;
 			Incidencia.pendientes--;
 		}
-		
-		
+
 	}
-	
-	public static int getPendiete()	{
+
+	public static int getPendiete() {
 		return Incidencia.pendientes;
 	}
 
@@ -78,12 +79,17 @@ public class Incidencia {
 
 	@Override
 	public String toString() {
-		return "Incidencia [codigo=" + codigo + ", estado=" + estado + ", problema=" + problema + ", solucion="
-				+ solucion + "]";
+		StringBuilder builder = new StringBuilder();
+		builder.append("Incidencia [codigo=");
+		builder.append(codigo);
+		builder.append(", estado=");
+		builder.append(estado);
+		builder.append(", problema=");
+		builder.append(problema);
+		builder.append(", solucion=");
+		builder.append(solucion);
+		builder.append("]");
+		return builder.toString();
 	}
-	
-	
-	
-	
-	
+
 }
--- Tema 4/src/Ejercicio8/IncidenciaAPP.java
+++ Tema 4/src/Ejercicio8/IncidenciaAPP.java
@@ -1,23 +1,33 @@
 package Ejercicio8;
 
+import java.util.Scanner;
+
 public class IncidenciaAPP {
 
 	public static void main(String[] args) {
 
-		Incidencia inc1 = new Incidencia("Roto el ascenso", "");
-		Incidencia inc2 = new Incidencia("Fuga de agua en el quinto b", "");
-		
+		Scanner sc = new Scanner(System.in);
+		String solucion;
+
+		Incidencia inc1 = new Incidencia("Roto el ascenso");
+		Incidencia inc2 = new Incidencia("Fuga de agua en el quinto b");
+
 		System.out.println(inc1);
 		System.out.println(inc2);
-		
-		System.out.println(Incidencia.getPendiete());
-		
+
+		System.out.println("Actualmente hay " + Incidencia.getPendiete() + " incidencias pendientes");
+
 		inc1.setEstado("En proceso");
-		inc1.resuelve();
-		inc2.resuelve();
-		
-		System.out.println(Incidencia.getPendiete());
-		
+
+
+		System.out.println("Escribe la solución para la incidencia con código " + inc1.getCodigo());
+		solucion = sc.nextLine();
+		inc1.resuelve(solucion);
+		System.out.println(inc1);
+
+		System.out.println("Ahora quedan " + Incidencia.getPendiete() + " incidencias por resolver");
+
+		sc.close();
 	}
 
 }
