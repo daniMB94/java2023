237fd862581f9eb3bfc5594dcf71d4ba40df0d89
DaniDAW <dmarber575@gmail.com>
2023-04-11 17:13:19 +0200

    comienzo T7E3

---
+++ Tema7/src/T7E3/Agenda.java
@@ -0,0 +1,68 @@
+package T7E3;
+
+import java.util.ArrayList;
+
+public class Agenda {
+
+	// ATRIBUTOS
+	private ArrayList<Contacto> contactos;
+
+	/**
+	 * @param contactos
+	 */
+	// CONSTRUCTOR
+	public Agenda() {
+		super();
+		this.contactos = new ArrayList<>();
+	}
+
+	// METODOS (Usaremos los métodos de la clase TreeMap)
+	/*
+	protected boolean existeContacto(Contacto c) {
+		int contador = 0;
+		for (Contacto contacto : contactos) {
+			if (contacto.getTelefono().equals(c.getTelefono())) {
+				contador = 1;
+			}
+
+		}
+		if (contador == 0)
+			return false;
+		else
+			return true;
+
+	}
+
+	protected void addContacto(Contacto c) {
+		contactos.add(c);
+	}
+
+	protected void listarContactos() {
+		for (Contacto contacto : contactos) {
+			System.out.println(contacto.toString());
+		}
+	}
+
+	protected void buscarContacto(String nombre) {
+		int contador = 0;
+		for (Contacto contacto : contactos) {
+			if (contacto.getNombre().equals(nombre)) {
+				System.out.println(contacto.getTelefono());
+				contador++;
+			}
+
+		}
+		if (contador == 0)
+			System.out.println("Contacto inexistente");
+	}
+
+	protected void eliminarContacto(Contacto c) {
+		contactos.remove(c);
+	}
+	
+	protected Contacto obtenerContacto(int posicionContacto) {
+		return contactos.get(posicionContacto);
+		
+	}
+	*/
+}
---
+++ Tema7/src/T7E3/AgendaAPP.java
@@ -0,0 +1,98 @@
+package T7E3;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class AgendaAPP {
+
+	public static void main(String[] args) {
+
+		Map<String, String> agenda = new TreeMap<>(new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				
+				return o1.compareTo(o2);
+			}
+			
+		});
+		
+		
+		
+		
+
+		Scanner sc = new Scanner(System.in);
+
+		int posicionEliminar = -1;
+		int selector = 0;
+		String nombre = "";
+		String telefono = "";
+
+		do {
+			System.out.println("AGENDA TELEFÓNICA");
+			System.out.println("*****************");
+			System.out.println("1. Añadir nuevo contacto");
+			System.out.println("2. Listar contactos");
+			System.out.println("3. Buscar contacto");
+			System.out.println("4. Eliminar contacto");
+			System.out.println("5. Estado de la agenda");
+			System.out.println("6. Salir");
+			System.out.println();
+			System.out.println("Elige una opción");
+			try {
+				selector = Integer.parseInt(sc.nextLine());
+			} catch (Exception e) {
+
+			}
+
+			switch (selector) {
+			case 1: {
+				System.out.println("1. Añadir nuevo contacto");
+				System.out.print("Introduce el nombre del contacto: ");
+				nombre = sc.nextLine();
+				System.out.print("Introduce el número del contacto: ");
+				telefono = sc.nextLine();
+				agenda.put(nombre, telefono);
+				nombre = "";
+				telefono = "";
+				break;
+			}
+			case 2: {
+				System.out.println("2. Listar contactos");
+				System.out.println(agenda.values());
+				break;
+			}
+			case 3: {
+				System.out.println("3. Buscar contactos");
+				System.out.print("Introduce el nombre a buscar");
+				nombre = sc.nextLine();
+				System.out.println(agenda.get(nombre));
+				break;
+			}
+			case 4: {
+				System.out.println("4. Eliminar contacto");
+				System.out.println("Introduce el nombre del contacto a eliminar: ");
+				System.out.println(agenda.keySet());
+				nombre = sc.nextLine();
+				agenda.remove(nombre);
+				System.out.println();
+				break;
+			}
+			case 5: {
+				System.out.println("2. Listar contactos");
+				System.out.println(agenda.values());
+				break;
+			}
+
+			case 6: {
+				System.out.println("6. Salir");
+			}
+
+			}
+		} while (selector != 6);
+
+	}
+
+}
