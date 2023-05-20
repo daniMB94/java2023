e66809f65424c7e5e8a4ce0af0552229db6b55de
dani_ <dani_aliast@hotmail.com>
2023-05-20 16:36:54 +0200

    act 20230520

---
+++ Tema7/src/practica3ejer3/TestFincas.java
@@ -0,0 +1,13 @@
+package practica3ejer3;
+
+public class TestFincas {
+
+	public static void main(String[] args) {
+
+		
+		DAOFinca df = new DAOFinca();
+		df.getFincasPorSuperficie();
+		
+	}
+
+}
